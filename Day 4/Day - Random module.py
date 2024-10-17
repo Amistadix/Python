83d9fb2c045a9ef7b0391f1bc3c07d6f7d6e80b5
amistadix <kiril@tavex.bg>
2024-10-17 14:14:44 +0300

    Random Generator

---
+++ Day 4/Day - Random module.py
@@ -0,0 +1,18 @@
+import random
+import my_module
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+# print(my_module.my_favorite_number)
+#
+# random_number_0_1 = random.random() * 10
+# print(random_number_0_1)
+
+random_float = random.uniform(1, 20)
+
+if random_float > 10:
+    print("Heads")
+else:
+    print("Tails")
+print(random_float)
+
